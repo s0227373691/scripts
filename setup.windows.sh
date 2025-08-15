f77ed9e86fb317cff3c166bb0c31c546e30f8167
ElmerPan <s0227373691@gms.tku.edu.tw>
2025-08-15 10:59:29 +0800

    update setup windows list

--- setup.windows.sh
+++ setup.windows.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-winget install --id ClickUp.ClickUp -e --source winget
-winget install --id Spotify.Spotify -e --source winget
-winget install --id Warp.Warp -e --source winget
-winget install --id TheBrowserCompany.Arc -e --source winget
+winget install -e --source winget --id AnyDesk.AnyDesk
+winget install -e --source winget --id Anysphere.Cursor
+winget install -e --source winget --id Brave.Brave
+winget install -e --source winget --id ClickUp.ClickUp
+winget install -e --source winget --id Docker.DockerDesktop
+winget install -e --source winget --id DeepL.DeepL
+winget install -e --source winget --id Git.Git
+winget install -e --source winget --id GitHub.GitHubDesktop
+winget install -e --source winget --id Google.Chrome.EXE
+winget install -e --source winget --id Spotify.Spotify
+winget install -e --source winget --id TheBrowserCompany.Arc
+winget install -e --source winget --id Tencent.WeChat.Universal
+winget install -e --source winget --id Microsoft.VisualStudioCode
+winget install -e --source winget --id Warp.Warp
+
+winget install -e --id MSIX\OpenAI.ChatGPT-Desktop_1.2025.202.0_x64__2p2nqsd0c76g0
