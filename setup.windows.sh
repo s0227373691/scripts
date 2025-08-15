800520b70bc3317ece2431f281eca44a65f8b8aa
ElmerPan <s0227373691@gms.tku.edu.tw>
2025-08-15 10:46:33 +0800

    chore: add install list

---
+++ setup.windows.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+winget install --id ClickUp.ClickUp -e --source winget
+winget install --id Spotify.Spotify -e --source winget
+winget install --id Warp.Warp -e --source winget
+winget install --id TheBrowserCompany.Arc -e --source winget
